d2eb16d94f7a15347a79765e09c76a95d0149893
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-10-20 21:19:31 +0200

    Added animations on every page

--- src/app/home/our-services/our-services.component.ts
+++ src/app/home/our-services/our-services.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-our-services',
   templateUrl: './our-services.component.html',
-  styleUrls: ['./our-services.component.scss']
+  styleUrls: ['./our-services.component.scss'],
+  animations: [routerTransition()]
 })
 export class OurServicesComponent implements OnInit {
 
